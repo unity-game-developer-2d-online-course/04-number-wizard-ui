5ef99bc4e240df633069c28e699498e4c5b56f13
Mattias Strömberg <m.stromberg@outlook.com>
2020-10-28 22:10:28 +0100

    Load all scenes

--- NumberWizardUI/Assets/Scripts/SceneLoader.cs
+++ NumberWizardUI/Assets/Scripts/SceneLoader.cs
@@ -3,6 +3,11 @@ using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
+    public void LoadStartScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+
     public void LoadNextScene()
     {
         int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
