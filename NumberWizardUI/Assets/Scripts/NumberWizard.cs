70f32e942dd1c3ea07900ffddb01917c5ad7b654
Mattias Strömberg <m.stromberg@outlook.com>
2020-10-30 22:42:54 +0100

    Import and refactor code

---
+++ NumberWizardUI/Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+namespace NumberWizard
+{
+    public class NumberWizard : MonoBehaviour
+    {
+        [SerializeField] private int _min;
+        [SerializeField] private int _max;
+        private int _guess;
+
+        // Start is called before the first frame update
+        private void Start()
+        {
+            StartGame();
+        }
+
+        private void StartGame()
+        {
+            _max++;
+            _guess = (_max + _min) / 2;
+        }
+
+        private void NextGuess()
+        {
+            _guess = (_max + _min) / 2;
+        }
+
+        public void OnPressHigher()
+        {
+            _min = _guess;
+            NextGuess();
+        }
+
+        public void OnPressLower()
+        {
+            _max = _guess;
+            NextGuess();
+        }
+    }
+}
